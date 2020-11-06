9aeb198d820840830a1745dd3ae9bfc0d2be750a
unknown <seba.olivaresa@alumnos.duoc.cl>
2020-11-06 17:34:20 -0300

    Creando Modelos

--- webjuegos/catalogo/models.py
+++ webjuegos/catalogo/models.py
@@ -4,20 +4,23 @@ import uuid
 
 # Create your models here.
 class Juego(models.Model):
-    titulo=models.CharField(max_length=200)
-    desarrollador=models.CharField(max_length=200)
-    genero=models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    desarrollador = models.CharField(max_length=200)
+    genero = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.title
 
-def __str__(self):
-    return self.titulo
+    def get_absolute_url(self):
+         return reverse('juego-detail', args=[str(self.id)])
 
-def get_absolute_url(self):
-    return reverse('juego-detail', args=[str(self.id)])
+    
 
 class JuegoInstance(models.Model):
     id= models.UUIDField(primary_key=True, default=uuid.uuid4)
     juego= models.ForeignKey('Juego',on_delete=models.SET_NULL, null=True)
     fecha_lan= models.DateField(null=True,blank=True)
 
-def __str__(self):
-		return f'{self.id} ({self.juego.titulo})'
+
+    def __str__(self):
+		    return f'{self.id} ({self.juego.title})'
