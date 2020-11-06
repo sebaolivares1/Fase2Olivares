41337ca511b6c00469ef02762406de0345114361
unknown <seba.olivaresa@alumnos.duoc.cl>
2020-11-05 22:32:27 -0300

    Creando Modelos

--- webjuegos/catalogo/models.py
+++ webjuegos/catalogo/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.urls import reverse  
+import uuid  
 
 # Create your models here.
+class Juego(models.Model):
+    titulo=models.CharField(max_length=200)
+    desarrollador=models.CharField(max_length=200)
+    genero=models.CharField(max_length=200)
+
+def __str__(self):
+    return self.titulo
+
+def get_absolute_url(self):
+    return reverse('juego-detail', args=[str(self.id)])
+
+class JuegoInstance(models.Model):
+    id= models.UUIDField(primary_key=True, default=uuid.uuid4)
+    juego= models.ForeignKey('Juego',on_delete=models.SET_NULL, null=True)
+    fecha_lan= models.DateField(null=True,blank=True)
+
+def __str__(self):
+		return f'{self.id} ({self.juego.titulo})'
