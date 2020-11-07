5f90b8e24b1e395a08b0e3e371654ba5e2f1d5ef
unknown <seba.olivaresa@alumnos.duoc.cl>
2020-11-07 00:06:49 -0300

    Creando Página de Inicio

--- webjuegos/catalogo/views.py
+++ webjuegos/catalogo/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from . models import Juego, JuegoInstance
+from django.views import generic
+
 
 # Create your views here.
+def index(request):
+    num_Juegos = Juego.objects.all().count()
+    num_Instances = JuegoInstance.objects.all().count()
+    
+    return render(
+        request,
+        'index.html',
+        context={'num_juegos': num_Juegos, 'num_instances': num_Instances,},
+    )
--- webjuegos/webjuegos/urls.py
+++ webjuegos/webjuegos/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalogo/',include('catalogo.urls'))
 ]
+
+urlpatterns+= static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
